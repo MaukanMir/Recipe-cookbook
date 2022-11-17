0164794fa7aedb573583dfd4e2bfba200cfe4744
Maukan Mir <e-source@E1069.local>
2022-11-17 15:53:23 -0500

    Added search feature

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,39 @@
 //React imports
-import React,{useState} from 'react'
+import {useState} from 'react';
 
 //styles
-import "./SearchBar.css"
+import "./SearchBar.css";
+
+//React-router-dom imports
+import {useHistory} from "react-router-dom";
 
 const SearchBar = () => {
-    
+    const [term, setTerm] = useState("");
+    const history = useHistory()
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        history.push(`/search?q=${term}`)
+
+    }
   return (
-    <div>SearchBar</div>
+    <div className="searchbar">
+        <form onSubmit={handleSubmit}>
+                <label htmlFor='search'>
+
+                    <input
+                    type="text"
+                    id="search"
+                    onChange={(e)=> setTerm(e.target.value)}
+                    required
+                    />
+
+                </label>
+        </form>
+
+    </div>
   )
 }
 
